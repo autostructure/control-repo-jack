63c2f9a50a8718b2a347721e39e4746ae91ccada
Jack Coleman <jackcoleman@Jacks-MacBook-Pro.local>
2017-07-06 17:21:13 -0400

    fixed error where java was declared twice

--- site/profile/manifests/java.pp
+++ site/profile/manifests/java.pp
@@ -3,7 +3,4 @@ class profile::java {
   include ::java
 
   #Make sure Java is up-to-date so that is compatible with Tomcat, which requires Java 6 or higher
-  class { 'java' :
-  package => 'java-1.8.0-openjdk-devel',
-  }
 }
